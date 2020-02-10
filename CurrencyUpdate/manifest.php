b2a47c8e9a037c89e2a12bd1ab91db7a6c6267db
Harald Kuske <hkuske@sugarcrm.com>
2020-02-10 21:49:33 +0100

    manifest
    manifest

--- CurrencyUpdate/manifest.php
+++ CurrencyUpdate/manifest.php
@@ -1,17 +1,17 @@
 <?php
 /*
   Currency_Update
- */
+*/
 
 $manifest = array (
-  'acceptable_sugar_versions' => 
-  array (
-    0 => '7.*.*',
-    1 => '8.*.*',
-    2 => '9.*.*',
+  'acceptable_sugar_versions' => array (
+      'regex_matches' => array(
+        '7.*.*',
+        '8.*.*',
+        '9.*.*',
+      ),
   ),
-  'acceptable_sugar_flavors' => 
-  array (
+  'acceptable_sugar_flavors' => array (
     0 => 'PRO',
     1 => 'CORP',
     2 => 'ENT',
@@ -26,18 +26,16 @@ $manifest = array (
   'name' => 'Currency_Update',
   'published_date' => '2019-05-01 00:00:00',
   'type' => 'module',
-  'version' => 9.0.0,
+  'version' => '202002',
   'remove_tables' => 'false',
 );
 
 $installdefs = array (
   'id' => 'Currency_Update',
-  'copy' => 
-  array (
-    0 => 
-    array (
-      'from' => '<basepath>/SugarModules/custom/',
-      'to' => 'custom/',
+  'copy' => array (
+    0 => array (
+      'from' => '<basepath>/SugarModules/custom/Extension/modules/Schedulers/Ext/ScheduledTasks/Update_Currencies.php',
+      'to' => 'custom/Extension/modules/Schedulers/Ext/ScheduledTasks/Update_Currencies.php',
     ),
   ),
 );
